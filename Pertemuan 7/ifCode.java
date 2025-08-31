817bf5658429eee11e5a0c099438124a589d7a51
Nur Hidayat <yayagm0712@gmail.com>
2025-08-31 23:03:21 +0800

    Delete Pertemuan 7 directory

--- Pertemuan 7/ifCode.java
+++
@@ -1,9 +0,0 @@
-public class ifCode {
-    public static void main(String[] args) {
-        int number = 10;
-
-        if (number > 0) {
-            System.out.println(number + " adalah angka positif.");
-        }
-    }
-}
